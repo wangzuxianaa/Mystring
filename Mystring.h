837ab43bd971331ff420821e746123a5d8863f0d
cyc <573553817@qq.com>
2022-01-12 22:13:05 +0800

    add some function

--- Mystring.h
+++ Mystring.h
@@ -9,7 +9,7 @@ using namespace std;
 class Mystring{
     friend ostream& operator<<(ostream& os,const Mystring& str);
     friend Mystring operator+(const Mystring& str1,const Mystring& str2);
-    friend Mystring operator-(const Mystring& str1,const Mystring& str2);
+    //friend Mystring operator-(const Mystring& str1,const Mystring& str2);
 public:
     Mystring();//默认构造
     Mystring(const char* cstr); //一般构造
@@ -20,17 +20,12 @@ public:
 
     //运算符重载
     Mystring& operator+=(const Mystring& str);
-    Mystring& operator-=(const Mystring& str);
-    // Mystring& operator+=(const int str_int);
-    // Mystring& operator-=(const int str_int);
-    // Mystring& operator+=(const double str_double);
-    // Mystring& operator-=(const double str_double);
+    //Mystring& operator-=(const Mystring& str);
 
     Mystring substr(int pos,const int n);
-    int find(const Mystring& str,int index);
     int toInt();
     double toDouble();
-    int length() const{return strlen(m_data);}
+    int length() const {return strlen(m_data);}
 private:
     char *m_data;
 };
